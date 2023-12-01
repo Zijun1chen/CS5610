efe0c2739c039fd345adc217df7d0b1dfd46aa24
Zijun1chen <116416216+Zijun1chen@users.noreply.github.com>
2023-12-01 14:39:11 -0800

    a5 REST fa23

---
+++ src/Labs/a5/index.js
@@ -0,0 +1,31 @@
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
+
+function Assignment5() {
+  return (
+    <div>
+      <h1>Assignment 5</h1>
+      <div className="list-group">
+        <a href="https://five610-node-caiirene.onrender.com/a5/welcome"
+           className="list-group-item">
+          Welcome
+        </a>
+      </div>
+
+      <hr />
+      <EncodingParametersInURLs />
+      <hr />
+      <hr />
+      <WorkingWithObjects />
+      <hr />
+      <hr />
+      <WorkingWithArrays />
+      <hr />
+      <hr />
+      
+    </div>
+  );
+}
+
+export default Assignment5;
